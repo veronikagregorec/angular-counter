136ea6cda57767953e2412a786d10d7fbb64e6e1
veronikagregorec <gregorec.veronika@gmail.com>
2023-03-18 17:46:27 +0100

    Functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as actions from './store/counter.actions'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-counter';
+export class AppComponent implements OnInit{
+
+  current$!: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>){}
+
+  increment() {
+    this.store.dispatch(actions.countIncremented())
+  }
+
+  decrement() {
+    this.store.dispatch(actions.countDecremented())
+  }
+
+  ngOnInit(): void {
+    this.current$ = this.store.select('count')
+  }
 }
