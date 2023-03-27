11722a6b2e47e705cee60f71683a82387027439c
veronikagregorec <gregorec.veronika@gmail.com>
2023-03-27 16:12:00 +0200

    Removed functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import * as actions from './store/counter.actions'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
-  current$!: Observable<number>;
-
-  constructor(private store: Store<{ count: number }>){}
-
-  increment() {
-    this.store.dispatch(actions.countIncremented())
-  }
-
-  decrement() {
-    this.store.dispatch(actions.countDecremented())
-  }
-
-  ngOnInit(): void {
-    this.current$ = this.store.select('count')
-  }
 }
