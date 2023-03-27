3d61a67ef81f875822457b2efcb11c26ac90fc72
veronikagregorec <gregorec.veronika@gmail.com>
2023-03-27 16:19:48 +0200

    Reducers

---
+++ src/app/store/actions/counter.actions.ts
@@ -0,0 +1,15 @@
+import { createAction, props } from '@ngrx/store';
+
+
+export const countIncremented = createAction(
+  '[counter] count incremented'
+)
+
+export const countDecremented = createAction(
+  '[counter] count decremented'
+)
+
+export const countSetCountBy = createAction(
+  '[counter] count set count by',
+  props<{ by: number }>()
+)
