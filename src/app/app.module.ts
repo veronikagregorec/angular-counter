0d9e2f73fc14a43849aa42ae9426912aae5710a9
veronikagregorec <gregorec.veronika@gmail.com>
2023-03-27 16:13:47 +0200

    Added declarations and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { counterReducer } from './store/counter.reducer';
+import { reducers } from './store/reducers';
+import { CounterComponent } from './components/counter/counter.component';
+import { CounterCountByComponent } from './components/counter-count-by/counter-count-by.component';
+import { CountersComponent } from './components/counters/counters.component';
+import { SingleComponent } from './components/single/single.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    CounterCountByComponent,
+    CountersComponent,
+    SingleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ count: counterReducer }),
+    ReactiveFormsModule,
+    StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument()
   ],
   providers: [],
